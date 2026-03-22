1fe2e40aad90fc2a8800218d0ce1541666329f6f
santoshsoni <santosh.soni02@gmail.com>
2026-03-22 15:02:00 +0530

    123 added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-####################Today Date : 22/03/2026###############santosh########################### 
+####################Today Date : 22/03/2026###############santosh123########################### 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import mysql.connector
 from werkzeug.security import check_password_hash
